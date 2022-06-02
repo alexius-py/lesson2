92354275075bfb2e80c52187ed989ead4e9d12d2
alexius <zte.lans@gmail.com>
2022-06-02 05:35:15 +0300

    draft

---
+++ borndayforewer.py
@@ -0,0 +1,15 @@
+year = ""
+day = ""
+while not year.isdigit():
+    year = input("Введите год рождения А. С. Пушкина: ")
+year = int(year)
+while year != 1799:
+    print("Неверно указан год")
+    year = int(input("Введите год рождения А. С. Пушкина: "))
+while not day.isdigit():
+    day = input("Введите день рождения А. С. Пушкина: ")
+day = int(day)
+while day != 26:
+    print("Неверно указан день")
+    day = int(input("Введите день рождения А. С. Пушкина: "))
+print("Верно")
---
+++ bornyearforewer.py
@@ -0,0 +1,8 @@
+year = ""
+while not year.isdigit():
+    year = input("Введите год рождения А. С. Пушкина: ")
+year = int(year)
+while year != 1799:
+    print("Неверно указан год")
+    year = int(input("Введите год рождения А. С. Пушкина: "))
+print("Верно")
